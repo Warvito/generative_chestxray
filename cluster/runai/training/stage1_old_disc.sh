3434f037789abc3d7f5d6e47a013fd6fb192d942
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-02-14 22:00:28 +0000

    Add old discriminator

---
+++ cluster/runai/training/stage1_old_disc.sh
@@ -0,0 +1,38 @@
+seed=42
+run_dir="aekl_v0_old_disc"
+training_ids="/project/outputs/ids/train_ids.tsv"
+validation_ids="/project/outputs/ids/val_ids.tsv"
+config_file="/project/configs/stage1/aekl_v0_old_disc.yaml"
+batch_size=48
+n_epochs=500
+eval_freq=3
+num_workers=32
+use_bfloat=0
+experiment="AEKL"
+
+runai submit \
+  --name mimic-aekl-old-disc-v0 \
+  --image aicregistry:5000/wds20:ldm_mimic \
+  --backoff-limit 0 \
+  --gpu 4 \
+  --cpu 16 \
+  --large-shm \
+  --run-as-user \
+  --node-type "A100" \
+  --host-ipc \
+  --project wds20 \
+  --volume /nfs/home/wds20/projects/generative_mimic/:/project/ \
+  --volume /nfs/home/wds20/datasets/MIMIC-CXR-JPG_v2.0.0/:/data/ \
+  --command -- bash /project/src/bash/start_script.sh \
+    python3 /project/src/python/training/train_aekl_old_disc.py \
+      seed=${seed} \
+      run_dir=${run_dir} \
+      training_ids=${training_ids} \
+      validation_ids=${validation_ids} \
+      config_file=${config_file} \
+      batch_size=${batch_size} \
+      n_epochs=${n_epochs} \
+      eval_freq=${eval_freq} \
+      num_workers=${num_workers} \
+      use_bfloat=${use_bfloat} \
+      experiment=${experiment}
---
+++ src/python/training/training_functions_old_disc.py
@@ -0,0 +1,310 @@
+from collections import OrderedDict
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from pynvml.smi import nvidia_smi
+from tensorboardX import SummaryWriter
+from torch.cuda.amp import GradScaler, autocast
+from tqdm import tqdm
+from util import log_reconstructions
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+
+def print_gpu_memory_report():
+    if torch.cuda.is_available():
+        nvsmi = nvidia_smi.getInstance()
+        data = nvsmi.DeviceQuery("memory.used, memory.total, utilization.gpu")["gpu"]
+        print("Memory report")
+        for i, data_by_rank in enumerate(data):
+            mem_report = data_by_rank["fb_memory_usage"]
+            print(f"gpu:{i} mem(%) {int(mem_report['used'] * 100.0 / mem_report['total'])}")
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# AUTOENCODER KL
+# ----------------------------------------------------------------------------------------------------------------------
+def train_aekl(
+    model: nn.Module,
+    discriminator: nn.Module,
+    perceptual_loss: nn.Module,
+    start_epoch: int,
+    best_loss: float,
+    train_loader: torch.utils.data.DataLoader,
+    val_loader: torch.utils.data.DataLoader,
+    optimizer_g: torch.optim.Optimizer,
+    optimizer_d: torch.optim.Optimizer,
+    n_epochs: int,
+    eval_freq: int,
+    writer_train: SummaryWriter,
+    writer_val: SummaryWriter,
+    device: torch.device,
+    run_dir: Path,
+    adv_weight: float,
+    perceptual_weight: float,
+    kl_weight: float,
+    use_bfloat: bool = False,
+) -> float:
+    scaler_g = GradScaler()
+    scaler_d = GradScaler()
+
+    raw_model = model.module if hasattr(model, "module") else model
+
+    val_loss = eval_aekl(
+        model=model,
+        discriminator=discriminator,
+        perceptual_loss=perceptual_loss,
+        loader=val_loader,
+        device=device,
+        step=len(train_loader) * start_epoch,
+        writer=writer_val,
+        kl_weight=kl_weight,
+        adv_weight=adv_weight,
+        perceptual_weight=perceptual_weight,
+        use_bfloat=use_bfloat,
+    )
+    print(f"epoch {start_epoch} val loss: {val_loss:.4f}")
+    for epoch in range(start_epoch, n_epochs):
+        train_epoch_aekl(
+            model=model,
+            discriminator=discriminator,
+            perceptual_loss=perceptual_loss,
+            loader=train_loader,
+            optimizer_g=optimizer_g,
+            optimizer_d=optimizer_d,
+            device=device,
+            epoch=epoch,
+            writer=writer_train,
+            kl_weight=kl_weight,
+            adv_weight=adv_weight,
+            perceptual_weight=perceptual_weight,
+            scaler_g=scaler_g,
+            scaler_d=scaler_d,
+            use_bfloat=use_bfloat,
+        )
+
+        if (epoch + 1) % eval_freq == 0:
+            val_loss = eval_aekl(
+                model=model,
+                discriminator=discriminator,
+                perceptual_loss=perceptual_loss,
+                loader=val_loader,
+                device=device,
+                step=len(train_loader) * epoch,
+                writer=writer_val,
+                kl_weight=kl_weight,
+                adv_weight=adv_weight,
+                perceptual_weight=perceptual_weight,
+                use_bfloat=use_bfloat,
+            )
+            print(f"epoch {epoch + 1} val loss: {val_loss:.4f}")
+            print_gpu_memory_report()
+
+            # Save checkpoint
+            checkpoint = {
+                "epoch": epoch + 1,
+                "state_dict": model.state_dict(),
+                "discriminator": discriminator.state_dict(),
+                "optimizer_g": optimizer_g.state_dict(),
+                "optimizer_d": optimizer_d.state_dict(),
+                "best_loss": best_loss,
+            }
+            torch.save(checkpoint, str(run_dir / "checkpoint.pth"))
+
+            if val_loss <= best_loss:
+                print(f"New best val loss {val_loss}")
+                best_loss = val_loss
+
+    print(f"Training finished!")
+    print(f"Saving final model...")
+    torch.save(raw_model.state_dict(), str(run_dir / "final_model.pth"))
+
+    return val_loss
+
+
+def train_epoch_aekl(
+    model: nn.Module,
+    discriminator: nn.Module,
+    perceptual_loss: nn.Module,
+    loader: torch.utils.data.DataLoader,
+    optimizer_g: torch.optim.Optimizer,
+    optimizer_d: torch.optim.Optimizer,
+    device: torch.device,
+    epoch: int,
+    writer: SummaryWriter,
+    kl_weight: float,
+    adv_weight: float,
+    perceptual_weight: float,
+    scaler_g: GradScaler,
+    scaler_d: GradScaler,
+    use_bfloat: bool = False,
+) -> None:
+    dtype = torch.bfloat16 if use_bfloat else None
+
+    model.train()
+    discriminator.train()
+
+    pbar = tqdm(enumerate(loader), total=len(loader))
+    for step, x in pbar:
+        images = x["image"].to(device)
+
+        # GENERATOR
+        optimizer_g.zero_grad(set_to_none=True)
+        with autocast(enabled=True, dtype=dtype):
+            reconstruction, z_mu, z_sigma = model(x=images)
+            l1_loss = F.l1_loss(reconstruction.float(), images.float())
+            p_loss = perceptual_loss(reconstruction.float(), images.float())
+
+            kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
+            kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+
+            logits_fake = discriminator(reconstruction.contiguous().float())
+            real_label = torch.ones_like(logits_fake, device=logits_fake.device)
+            generator_loss = F.mse_loss(logits_fake, real_label)
+
+            loss = l1_loss + kl_weight * kl_loss + perceptual_weight * p_loss + adv_weight * generator_loss
+
+            loss = loss.mean()
+            l1_loss = l1_loss.mean()
+            p_loss = p_loss.mean()
+            kl_loss = kl_loss.mean()
+            g_loss = generator_loss.mean()
+
+            losses = OrderedDict(
+                loss=loss,
+                l1_loss=l1_loss,
+                p_loss=p_loss,
+                kl_loss=kl_loss,
+                g_loss=g_loss,
+            )
+
+        scaler_g.scale(losses["loss"]).backward()
+        scaler_g.unscale_(optimizer_g)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+        scaler_g.step(optimizer_g)
+        scaler_g.update()
+
+        # DISCRIMINATOR
+        optimizer_d.zero_grad(set_to_none=True)
+
+        with autocast(enabled=True, dtype=dtype):
+            logits_fake = discriminator(reconstruction.contiguous().detach())
+            fake_label = torch.zeros_like(logits_fake, device=logits_fake.device)
+            loss_d_fake = F.mse_loss(logits_fake, fake_label)
+            logits_real = discriminator(images.contiguous().detach())
+            real_label = torch.ones_like(logits_real, device=logits_real.device)
+            loss_d_real = F.mse_loss(logits_real, real_label)
+            discriminator_loss = (loss_d_fake + loss_d_real) * 0.5
+
+            d_loss = adv_weight * discriminator_loss
+            d_loss = d_loss.mean()
+
+        scaler_d.scale(d_loss).backward()
+        scaler_d.unscale_(optimizer_d)
+        torch.nn.utils.clip_grad_norm_(discriminator.parameters(), 1)
+        scaler_d.step(optimizer_d)
+        scaler_d.update()
+
+        losses["d_loss"] = discriminator_loss
+
+        writer.add_scalar("lr_g", get_lr(optimizer_g), epoch * len(loader) + step)
+        writer.add_scalar("lr_d", get_lr(optimizer_d), epoch * len(loader) + step)
+        for k, v in losses.items():
+            writer.add_scalar(f"{k}", v.item(), epoch * len(loader) + step)
+
+        pbar.set_postfix(
+            {
+                "epoch": epoch,
+                "loss": f"{losses['loss'].item():.6f}",
+                "l1_loss": f"{losses['l1_loss'].item():.6f}",
+                "p_loss": f"{losses['p_loss'].item():.6f}",
+                "g_loss": f"{losses['g_loss'].item():.6f}",
+                "d_loss": f"{losses['d_loss'].item():.6f}",
+                "lr_g": f"{get_lr(optimizer_g):.6f}",
+                "lr_d": f"{get_lr(optimizer_d):.6f}",
+            },
+        )
+
+
+@torch.no_grad()
+def eval_aekl(
+    model: nn.Module,
+    discriminator: nn.Module,
+    perceptual_loss: nn.Module,
+    loader: torch.utils.data.DataLoader,
+    device: torch.device,
+    step: int,
+    writer: SummaryWriter,
+    kl_weight: float,
+    adv_weight: float,
+    perceptual_weight: float,
+    use_bfloat: bool = False,
+) -> float:
+    model.eval()
+    discriminator.eval()
+
+    total_losses = OrderedDict()
+    for x in loader:
+        images = x["image"].to(device)
+
+        with autocast(enabled=True, dtype=torch.bfloat16 if use_bfloat else None):
+            # GENERATOR
+            reconstruction, z_mu, z_sigma = model(x=images)
+            l1_loss = F.l1_loss(reconstruction.float(), images.float())
+            p_loss = perceptual_loss(reconstruction.float(), images.float())
+            kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
+            kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+
+            logits_fake = discriminator(reconstruction.contiguous().float())
+            real_label = torch.ones_like(logits_fake, device=logits_fake.device)
+            generator_loss = F.mse_loss(logits_fake, real_label)
+
+            # DISCRIMINATOR
+            logits_fake = discriminator(reconstruction.contiguous().detach())
+            fake_label = torch.zeros_like(logits_fake, device=logits_fake.device)
+            loss_d_fake = F.mse_loss(logits_fake, fake_label)
+            logits_real = discriminator(images.contiguous().detach())
+            real_label = torch.ones_like(logits_real, device=logits_real.device)
+            loss_d_real = F.mse_loss(logits_real, real_label)
+            discriminator_loss = (loss_d_fake + loss_d_real) * 0.5
+
+            loss = l1_loss + kl_weight * kl_loss + perceptual_weight * p_loss + adv_weight * generator_loss
+
+            loss = loss.mean()
+            l1_loss = l1_loss.mean()
+            p_loss = p_loss.mean()
+            kl_loss = kl_loss.mean()
+            g_loss = generator_loss.mean()
+            d_loss = discriminator_loss.mean()
+
+            losses = OrderedDict(
+                loss=loss,
+                l1_loss=l1_loss,
+                p_loss=p_loss,
+                kl_loss=kl_loss,
+                g_loss=g_loss,
+                d_loss=d_loss,
+            )
+
+        for k, v in losses.items():
+            total_losses[k] = total_losses.get(k, 0) + v.item() * images.shape[0]
+
+    for k in total_losses.keys():
+        total_losses[k] /= len(loader.dataset)
+
+    for k, v in total_losses.items():
+        writer.add_scalar(f"{k}", v, step)
+
+    log_reconstructions(
+        image=images,
+        reconstruction=reconstruction,
+        writer=writer,
+        step=step,
+    )
+
+    return total_losses["l1_loss"]
