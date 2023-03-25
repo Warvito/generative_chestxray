5320153c4500b5ceedfb02ee76cc9beb4a6491e9
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-03-25 16:24:14 +0000

    Add utils for testing

---
+++ src/python/testing/util.py
@@ -0,0 +1,63 @@
+"""Utility functions for testing."""
+
+import pandas as pd
+from monai import transforms
+from monai.data import Dataset
+from torch.utils.data import DataLoader
+
+
+def get_test_dataloader(
+    batch_size: int,
+    test_ids: str,
+    num_workers: int = 8,
+):
+    test_transforms = transforms.Compose(
+        [
+            transforms.LoadImaged(keys=["image"]),
+            transforms.EnsureChannelFirstd(keys=["image"]),
+            transforms.Lambdad(
+                keys=["image"],
+                func=lambda x: x[0, :, :][
+                    None,
+                ],
+            ),
+            transforms.Rotate90d(keys=["image"], k=-1, spatial_axes=(0, 1)),  # Fix flipped image read
+            transforms.Flipd(keys=["image"], spatial_axis=1),  # Fix flipped image read
+            transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True),
+            transforms.CenterSpatialCropd(keys=["image"], roi_size=(512, 512)),
+            transforms.ToTensord(keys=["image"]),
+        ]
+    )
+
+    test_dicts = get_datalist(ids_path=test_ids, extended_report=False)
+    test_ds = Dataset(data=test_dicts, transform=test_transforms)
+    test_loader = DataLoader(
+        test_ds,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        drop_last=False,
+        pin_memory=False,
+        persistent_workers=True,
+    )
+
+    return test_loader
+
+
+def get_datalist(
+    ids_path: str,
+    extended_report: bool = False,
+):
+    """Get data dicts for data loaders."""
+    df = pd.read_csv(ids_path, sep="\t")
+
+    data_dicts = []
+    for index, row in df.iterrows():
+        data_dicts.append(
+            {
+                "image": f"{row['image']}",
+            }
+        )
+
+    print(f"Found {len(data_dicts)} subjects.")
+    return data_dicts
