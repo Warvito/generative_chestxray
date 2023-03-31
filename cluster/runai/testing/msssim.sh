7d0f477205e6d6d53f587eee948680e43c0ff616
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-03-31 16:42:47 +0100

    Fix  msssim

--- cluster/runai/testing/msssim.sh
+++ cluster/runai/testing/msssim.sh
@@ -1,4 +1,24 @@
 seed=42
+sample_dir="/project/outputs/samples_unconditioned/"
+num_workers=8
+
+runai submit \
+  --name  mimic-ssim-sample \
+  --image aicregistry:5000/wds20:ldm_mimic \
+  --backoff-limit 0 \
+  --gpu 1 \
+  --cpu 4 \
+  --large-shm \
+  --run-as-user \
+  --host-ipc \
+  --project wds20 \
+  --volume /nfs/home/wds20/projects/generative_mimic/:/project/ \
+  --command -- python3 /project/src/python/testing/compute_msssim_sample.py \
+      --seed=${seed} \
+      --sample_dir=${sample_dir} \
+      --num_workers=${num_workers}
+
+seed=42
 test_ids="/project/outputs/ids/test.tsv"
 num_workers=8
 
