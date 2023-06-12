714b1205c27f3c4c4ccacba63e70bffcd5adde02
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-06-12 19:27:55 +0100

    Update ldm config

--- cluster/runai/training/ldm.sh
+++ cluster/runai/training/ldm.sh
@@ -1,23 +1,23 @@
 seed=2
-run_dir="aekl_v0_ldm_v0"
+run_dir="aekl_v3_ldm_v3"
 training_ids="/project/outputs/ids/train.tsv"
 validation_ids="/project/outputs/ids/validation.tsv"
-stage1_uri="/project/mlruns/398344666374521908/0fde76e3e71b4ed4a92aea593c73c3db/artifacts/final_model"
-config_file="/project/configs/ldm/ldm_v0.yaml"
+stage1_uri="/project/mlruns/398344666374521908/d21dfcf5ba6d424dad18994bec47af29/artifacts/final_model"
+config_file="/project/configs/ldm/ldm_v3.yaml"
 scale_factor=0.3
-batch_size=256
-n_epochs=500
+batch_size=128
+n_epochs=1000
 eval_freq=10
-num_workers=64
+num_workers=128
 extended_report=0
 experiment="LDM"
 
 runai submit \
-  --name  mimic-ldm-v0 \
+  --name  mimic-ldm-v3 \
   --image aicregistry:5000/wds20:ldm_mimic \
   --backoff-limit 0 \
   --gpu 8 \
-  --cpu 32 \
+  --cpu 64 \
   --large-shm \
   --run-as-user \
   --host-ipc \
